c4387faadf1820448da8fd625e96792ade9bbfa3
金雯 <554050334@qq.com>
2018-09-11 22:54:21 +0800

    add Single number algorithm

---
+++ src/SingleNumber.java
@@ -0,0 +1,38 @@
+public class SingleNumber {
+
+//    Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+//
+//    Note:
+//
+//    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+//
+//    Example 1:
+//
+//    Input: [2,2,1]
+//    Output: 1
+//    Example 2:
+//
+//    Input: [4,1,2,1,2]
+//    Output: 4
+
+
+    public static int singleNumber(int[] nums){
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i++){
+            result = result ^ nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String args[]) throws Exception{
+        int[] nums = new int[]{2,2,1};
+        System.out.println(singleNumber(nums));
+
+        int[] nums2 = new int[]{4,1,2,1,2};
+        System.out.println(singleNumber(nums2));
+    }
+
+
+}
